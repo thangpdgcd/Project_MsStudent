466b3c33421cb1eddfeab8c4eb738bbd99901f4f
thangpdgcd <thangpdgcd201784@fpt.edu.vn>
2024-08-25 11:01:08 +0700

    update code

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -98,7 +98,7 @@ let getDetaildoctorbyId = (inputId) => {
                     },
                     attributes:
                     {
-                        exclude: ["password", "image"],
+                        exclude: ["password"],
                     },
                     include: [
                         {
@@ -107,9 +107,15 @@ let getDetaildoctorbyId = (inputId) => {
                         },
                         { model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi'] },
                     ],
-                    raw: true,
+                    raw: false,
                     nest: true
                 });
+                if (data && data.image) {
+                    data.image = new Buffer(data.image, 'base64').toString('binary')
+                }
+                if (!data) {
+                    data.image = {};
+                };
                 resolve({
                     errCode: 0,
                     data: data,
