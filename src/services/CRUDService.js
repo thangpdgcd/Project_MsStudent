1ca4b65486ed26cfaa1c6047b360280197a6107b
thangpdgcd <thangpdgcd201784@fpt.edu.vn>
2024-08-29 12:10:02 +0700

    update code

--- src/services/CRUDService.js
+++ src/services/CRUDService.js
@@ -14,7 +14,7 @@ let createNewUser = async (data) => {
         lastName: data.lastName,
         address: data.address,
         phonenumber: data.phonenumber,
-        gender: data.gender === "1" ? true : false,
+        gender: data.gender,
         roleId: data.roleId,
       });
       resolve("create new user succeed");
