4130cae8a8bde431ba7fd21de61f133a1ecc692f
thangpdgcd <thangpdgcd201784@fpt.edu.vn>
2024-10-10 15:56:20 +0700

    update doctor extra detail

--- src/containers/Patient/Doctor/DoctorExtraInfor.js
+++ src/containers/Patient/Doctor/DoctorExtraInfor.js
@@ -2,13 +2,15 @@ import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import "./DoctorExtraInfor.scss";
 import { LANGUAGES } from "../../../utils";
-import { getScheduleDoctorByDate, handleLoginApi } from "../../../services/userService";
+import { getExtraInforDoctorById } from "../../../services/userService";
 import { FormattedMessage } from "react-intl";
+import NumberFormat from "react-number-format";
 class DoctorExtraInfor extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isShowDetailInfor: false
+            isShowDetailInfor: false,
+            extraInfor: {}
         }
     }
 
@@ -18,6 +20,14 @@ class DoctorExtraInfor extends Component {
     async componentDidUpdate(prevProps, prevState, snapshot) {
         if (this.props.language !== prevProps.language) {
         }
+        if (this.props.doctorIdFromParents !== prevProps.doctorIdFromParents) {
+            let res = await getExtraInforDoctorById(this.props.doctorIdFromParents);
+            if (res && res.errCode === 0) {
+                this.setState({
+                    extraInfor: res.data
+                })
+            }
+        }
     }
     showHideDetailInfor = (status) => {
         this.setState({
@@ -26,7 +36,9 @@ class DoctorExtraInfor extends Component {
     }
 
     render() {
-        let { isShowDetailInfor } = this.state
+        let { isShowDetailInfor, extraInfor } = this.state
+        let { language } = this.props;
+        console.log("checkssss dataaaaasss", this.state)
         return (
             <div className="doctor-extra-infor-container">
                 <div className="content-up">
@@ -34,36 +46,96 @@ class DoctorExtraInfor extends Component {
 
                     </div >
                     <div className="name-clinic">
-
+                        {extraInfor && extraInfor.nameClinic ?
+                            extraInfor.nameClinic : ''
+                        }
                     </div >
                     <div className="detail-address">
-
+                        {extraInfor && extraInfor.addressClinic ?
+                            extraInfor.addressClinic : ''
+                        }
                     </div >
                 </div >
                 <div className="content-down">
+                    {/* component NumberFormat định giá cho price */}
                     {isShowDetailInfor === false &&
                         <div className="short-infor">
-                            <span onClick={() => this.showHideDetailInfor(true)}>
-                                xxem chi tiet
+                            <FormattedMessage id="patient.doctor-extra-infor.content-down.short-infor" />
+                            {extraInfor && extraInfor.priceTypeData && language === LANGUAGES.VI
+                                &&
+                                < NumberFormat
+                                    className="currency"
+                                    value={extraInfor.priceTypeData.valueVi}
+                                    displayType={"text"}
+                                    thousandSeparator={true}
+                                    suffix={`VND`} />
+
+                            }
+                            {extraInfor && extraInfor.priceTypeData && language === LANGUAGES.EN
+                                &&
+                                < NumberFormat
+                                    className="currency"
+                                    value={extraInfor.priceTypeData.valueEn}
+                                    displayType={"text"}
+                                    thousandSeparator={true}
+                                    suffix={`$`} />
+                            }
+                            <span className="detail" onClick={() => this.showHideDetailInfor(true)}>
+                                <FormattedMessage id="patient.doctor-extra-infor.content-down.detail" />
                             </span>
                         </div >
                     }
                     {isShowDetailInfor === true &&
                         <>
-                            <div className="title-price"></div >
+                            <div className="title-price"><FormattedMessage id="patient.doctor-extra-infor.content-down.short-infor" /></div >
                             <div className="detail-infor">
                                 <div className="price">
-                                    <span className="left"></span >
-                                    <span className="right"></span >
+                                    <span className="left"><FormattedMessage id="patient.doctor-extra-infor.content-down.short-infor" /></span >
+                                    <span className="right">
+                                        {extraInfor && extraInfor.priceTypeData && language === LANGUAGES.VI
+                                            &&
+                                            < NumberFormat
+                                                className="currency"
+                                                value={extraInfor.priceTypeData.valueVi}
+                                                displayType={"text"}
+                                                thousandSeparator={true}
+                                                suffix={`VND`} />
+
+                                        }
+                                        {extraInfor && extraInfor.priceTypeData && language === LANGUAGES.EN
+                                            &&
+                                            < NumberFormat
+                                                className="currency"
+                                                value={extraInfor.priceTypeData.valueEn}
+                                                displayType={"text"}
+                                                thousandSeparator={true}
+                                                suffix={`$`} />
+                                        }
+                                    </span >
                                 </div>
                                 <div className="note">
+                                    <FormattedMessage id="patient.doctor-extra-infor.content-down.note" />
+                                    {extraInfor && extraInfor.note ?
+                                        extraInfor.note : ''
+                                    }
                                 </div>
                             </div >
-                            <div className="payment"></div >
+                            <div className="payment">
+                                <FormattedMessage id="patient.doctor-extra-infor.content-down.payment" />
+
+                                {extraInfor && extraInfor.paymentTypeData && language === LANGUAGES.VI
+                                    ?
+                                    extraInfor.paymentTypeData.valueVi : ''
+                                }
+                                {extraInfor && extraInfor.paymentTypeData && language === LANGUAGES.EN
+                                    ?
+                                    extraInfor.paymentTypeData.valueEn : ''
+                                }
+                            </div >
 
                             <div className="hide-price">
                                 <span onClick={() => this.showHideDetailInfor(false)}>
-                                    an ban gias
+                                    <FormattedMessage id="patient.doctor-extra-infor.content-down.hide-price" />
                                 </span>
                             </div >
                         </>
--- src/services/userService.js
+++ src/services/userService.js
@@ -54,6 +54,11 @@ const getScheduleDoctorByDate = (doctorId, date) => {
     `/api/get-schedule-doctor-by-date?doctorId=${doctorId}&date=${date}`
   );
 };
+const getExtraInforDoctorById = (doctorId) => {
+  return axios.get(
+    `/api/get-extra-infor-doctor-by-id?doctorId=${doctorId}`
+  );
+};
 export {
   handleLoginApi,
   getAllUsers,
@@ -67,4 +72,5 @@ export {
   getDetailInforDoctor,
   saveBulkScheduleDoctor,
   getScheduleDoctorByDate,
+  getExtraInforDoctorById
 };
